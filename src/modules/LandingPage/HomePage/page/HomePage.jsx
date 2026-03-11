d603d37a7b5c24f8e91015ca1494be226a644752
Ahmed Elwaraky <113851356+ahmedelwaraky@users.noreply.github.com>
2026-03-11 03:08:16 +0200

    translation

--- src/modules/LandingPage/HomePage/page/HomePage.jsx
+++ src/modules/LandingPage/HomePage/page/HomePage.jsx
@@ -5,11 +5,10 @@ import AboutSection from "../component/AboutSection";
 import ServicesSection from "../component/servicesSection";
 import PartnersSection from "../component/PartnersSection";
 import NewsSection from "../component/NewsSection";
-import TestimonialsSection from "../component/Testimonialssection";
 import Footer from "../../../../components/layout/Footer";
 import CoursesSection from "../component/CoursesSection";
 import ContactSection from "../component/ContactSection";
-import TestimonialsSection from "../component/Testimonialssection";
+import  TestimonialsSection from "../component/TestimonialsSection";
 
 export default function HomePage() {
   return (
