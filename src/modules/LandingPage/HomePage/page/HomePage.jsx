6f1de219f3b9d0e498d3224c0bf6e1a5bd57e682
Ahmed Elwaraky <113851356+ahmedelwaraky@users.noreply.github.com>
2026-03-11 03:16:28 +0200

    translation

--- src/modules/LandingPage/HomePage/page/HomePage.jsx
+++ src/modules/LandingPage/HomePage/page/HomePage.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import Navbar from "../../../../components/layout/Navbar";
 import HeroSection from "../component/HeroSection";
-import AboutSection from "../component/AboutSection";
-import ServicesSection from "../component/servicesSection";
-import NewsSection from "../component/NewsSection";
 import Footer from "../../../../components/layout/Footer";
-import ContactSection from "../component/ContactSection";
-import  TestimonialsSection from "../component/TestimonialsSection";
+
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <HeroSection />
-      <AboutSection />
-      <ServicesSection />
-      <NewsSection />
-      <TestimonialsSection />
-      <ContactSection />
+   
       <Footer />
     </>
   );
